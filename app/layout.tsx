9b4acc0575a33e1e8d1c02badbf093ea0e9e23ef
komradefoods1025-max <komrade.foods1025@gmail.com>
2026-04-10 01:33:05 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
 
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "かむらど予約Web",
+  description: "かむらどのお弁当予約Webアプリ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
